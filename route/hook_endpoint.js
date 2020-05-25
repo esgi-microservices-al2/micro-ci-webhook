f93038d0a010ff3c65ba053029d1e37a710b9805
GautierL4 <gautier.laurent4@gmail.com>
2020-05-25 13:54:26 +0200

    extract data from github payload

--- route/hook_endpoint.js
+++ route/hook_endpoint.js
@@ -30,4 +30,35 @@ router.post('/', async (req, res) => {
 
 });
 
+router.post('/github', async(req, res) => {
+    console.log("Request from github received : ", req.body);
+    let branchName = req.body.ref.toString().split('/').pop();
+    let output = {
+        repository: {
+            fullname: req.body.repository.full_name,
+            name: req.body.repository.name,
+            private: req.body.repository.private,
+            url: req.body.repository.url,
+            clone_url: req.body.repository.clone_url,
+            default_branch: req.body.repository.default_branch
+        },
+        commit: [ ],
+        branch: branchName,
+        user: {
+            username: req.body.pusher.name,
+            email: req.body.pusher.email,
+            avatar: req.body.sender.avatar_url
+        },
+        date: req.body.head_commit.timestamp
+    };
+    req.body.commits.forEach(element => output.commit.push(element));
+    console.log("Output object for RabbitMQ (JSON) : ", output);
+    return res.status(200).end();
+});
+
+router.post('/gitlab', async(req, res) => {
+    console.log("Request from gitlab received : ", req.body);
+    return res.status(200).end();
+})
+
 module.exports = router;
