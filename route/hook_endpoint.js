c001cd6c2e38b2950f51a62af250c56ea77b5565
GautierL4 <gautier.laurent4@gmail.com>
2020-05-25 20:50:10 +0200

    Add gitlab endpoint

--- route/hook_endpoint.js
+++ route/hook_endpoint.js
@@ -37,27 +37,46 @@ router.post('/github', async(req, res) => {
         repository: {
             fullname: req.body.repository.full_name,
             name: req.body.repository.name,
-            private: req.body.repository.private,
             url: req.body.repository.url,
             clone_url: req.body.repository.clone_url,
             default_branch: req.body.repository.default_branch
         },
-        commit: [ ],
+        commits: [ ],
         branch: branchName,
         user: {
             username: req.body.pusher.name,
             email: req.body.pusher.email,
             avatar: req.body.sender.avatar_url
         },
-        date: req.body.head_commit.timestamp
+        timestamp: Date.now()
     };
-    req.body.commits.forEach(element => output.commit.push(element));
+    req.body.commits.forEach(element => output.commits.push(element));
     console.log("Output object for RabbitMQ (JSON) : ", output);
     return res.status(200).end();
 });
 
 router.post('/gitlab', async(req, res) => {
     console.log("Request from gitlab received : ", req.body);
+    let branchName = req.body.ref.toString().split('/').pop();
+    let output = {
+        repository: {
+            fullname: req.body.project.path_with_namespace,
+            name: req.body.project.name,
+            url: req.body.project.web_url,
+            clone_url: req.body.project.http_url,
+            default_branch: req.body.project.default_branch
+        },
+        commits: [ ],
+        branch: branchName,
+        user: {
+            username: req.body.user_username,
+            email: req.body.user_email,
+            avatar: req.body.user_avatar
+        },
+        timestamp: Date.now()
+    };
+    req.body.commits.forEach(element => output.commits.push(element));
+    console.log("Output object for RabbitMQ (JSON) : ", output);
     return res.status(200).end();
 })
 
