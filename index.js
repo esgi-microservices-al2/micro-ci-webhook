8269fc5a3fa1c1cb3a8f39f86b5cd539b9291ff1
Alexandre CHAMPAUD <alex.bis8@hotmail.fr>
2020-06-11 21:52:20 +0200

    Add broker ip env

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.use(morgan('dev'));
 
 routerBuilder.build(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.WEBHOOK_PORT || 3000;
 app.listen(port, () => console.log(`Server listening on ${port}...`));
